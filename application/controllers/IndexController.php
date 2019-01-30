2dd330c1eb9a2ac2f5539a40895a68d0f0dcaee8
dougtorres <douglastorr@gmail.com>
2019-01-30 18:28:43 -0300

    criação de models, adapters e primeiro método

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,20 +1,68 @@
 <?php
 
-class IndexController extends Zend_Controller_Action
-{
+class IndexController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
+        $this->logger = Zend_Registry::get('logger');
     }
 
-    public function indexAction()
-    {
-       $dbMarca = new Application_Model_DbTable_Marca();
-       $result = $dbMarca->fetchAll();
-        echo $result->name;
-    }
+    public function indexAction() {
+        $marcasJson = file_get_contents('http://fipeapi.appspot.com/api/1/carros/marcas.json');
+        $marcas = Zend_Json::decode($marcasJson);
+        $carrosJson = file_get_contents('http://fipeapi.appspot.com/api/1/carros/veiculos/21.json');
+        $carros= Zend_Json::decode($carrosJson);
+        $dbMarca = new Application_Model_DbTable_Marca();
+        print_r();
+        $this->logger->log($dbMarca, Zend_log::DEBUG);
+        $configuration = new Zend_Config_Ini(
+                APPLICATION_PATH . '\configs\application.ini', 'production'
+        );
+        $params = $configuration->resources->db->params->toArray();
+        $DB = new Zend_Db_Adapter_Pdo_Mysql($params);
+        foreach ($marcas as $row) {
+            $validator = new Zend_Validate_Db_RecordExists(
+                    array(
+                'table' => 'marca',
+                'field' => 'id',
+                'adapter' => $DB
+                    )
+            );
+            if (!$validator->isValid($row["id"])) {
+                $novaMarca = array(
+                    "id" => $row["id"],
+                    "name" => $row["name"],
+                    "fipe_name" => $row["fipe_name"],
+                    "order" => $row["order"],
+                    "key" => $row["key"],
+                );
+                $dbMarca->insert($novaMarca);
+                $this->logger->log("Achei", Zend_log::DEBUG);
+            } else
+                $this->logger->log("Achei", Zend_log::DEBUG);
+        }
 
+        foreach ($carros as $row) {
+            $validator = new Zend_Validate_Db_RecordExists(
+                    array(
+                'table' => 'carro',
+                'field' => 'id',
+                'adapter' => $DB
+                    )
+            );
+            if (!$validator->isValid($row["id"])) {
+                $novoCarro = array(
+                    "id" => $row["id"],
+                    "name" => $row["name"],
+                    "fipe_name" => $row["fipe_name"],
+                    "order" => $row["order"],
+                    "key" => $row["key"],
+                );
+                $dbMarca->insert($novaMarca);
+                $this->logger->log("Achei", Zend_log::DEBUG);
+            } else
+                $this->logger->log("Achei", Zend_log::DEBUG);
+        }
+    }
 
 }
-
--- application/models/DbTable/Marca.php
+++ application/models/DbTable/Marca.php
@@ -4,7 +4,13 @@ class Application_Model_DbTable_Marca extends Zend_Db_Table_Abstract
 {
 
     protected $_name = 'marca';
-
+    
+     public function getMarcas()
+    {
+        $select = $this->select()
+                    ->order("name asc");
+        return $this->fetchAll($select)->toArray();
+    }
 
 }
 
